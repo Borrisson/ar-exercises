c4978c23e5b0de4eaa43cb52f98c2b0d028d880e
Borrisson <lucborris@gmail.com>
2021-03-01 17:53:55 +0000

    Add create logic to add store properties

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,11 @@
-require_relative '../setup'
+require_relative "../setup"
 
 puts "Exercise 1"
 puts "----------"
+# ApplicationRecord for rails 5? using rails 5.1.4?
+class Store < ActiveRecord::Base
+end
 
-# Your code goes below here ...
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
