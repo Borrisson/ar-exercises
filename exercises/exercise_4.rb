94c8d243a74164a79dc50d34aaa7e89e528bd879
Borrisson <lucborris@gmail.com>
2021-03-01 19:49:31 +0000

    Add each loop to print out values of @mens_store

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -11,4 +11,11 @@ Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens
 Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
 Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
+@mens_store = Store.where("mens_apparel = true")
+
+@mens_store.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
 p Store.count
