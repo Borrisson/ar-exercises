8439cc06698a7d6c5f6710f59bb78a2fcce95bd9
Luc <lucborris@gmail.com>
2021-03-01 17:25:42 -0500

    Add employees to store instance variables

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,3 +11,7 @@ puts "----------"
 class Employees < ActiveRecord::Base
   belongs_to :store
 end
+
+@store1.employees.create(first_name: "Luc", last_name: "Borris", hourly_rate: 0)
+@store2.employees.create(first_name: "Caroline", last_name: "Aube", hourly_rate: 0)
+@store2.employees.create(first_name: "Melanie", last_name: "Herbert", hourly_rate: 0)
