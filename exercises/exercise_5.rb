8ecb4959dcf180d30bcffc8e763a77b62fe228b0
Borrisson <lucborris@gmail.com>
2021-03-01 20:03:13 +0000

    Add query for stores who make more than 1M

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,6 @@ require_relative "./exercise_4"
 puts "Exercise 5"
 puts "----------"
 
-p Store.all.sum(:annual_revenue)
+puts "Sum", Store.all.sum(:annual_revenue)
+puts "Avg", Store.all.average(:annual_revenue)
+puts "# of stores who make more that $1M in annual sales", Store.where("annual_revenue > 1000000").size
