6d6f057e787e79ada698761155a9b79fae1a2187
Borrisson <lucborris@gmail.com>
2021-03-01 19:27:55 +0000

    Add destroy method to store3 instance variable

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -1,8 +1,10 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
 
 puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3.destroy
+puts @store3, Store.count
