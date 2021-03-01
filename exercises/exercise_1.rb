f0443d818645d8d2190656dab2c530a5b8ec9df1
Luc <lucborris@gmail.com>
2021-03-01 17:49:02 -0500

    Add validations to classes

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,6 +5,15 @@ puts "----------"
 # ApplicationRecord for rails 5? using rails 5.1.4?
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, :inclusion => 0..Float::INFINITY
+  validate :has_atleast_one_gender_apperal
+
+  def has_atleast_one_gender_apperal
+    if !mens_apparel && !womens_apparel
+      errors.add(:apparel, "Must have atleast one line of clothing")
+    end
+  end
 end
 
 Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -10,8 +10,12 @@ puts "----------"
 
 class Employees < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :store_id, presence: true
+  validates :hourly_rate, :inclusion => 40..200
 end
 
-@store1.employees.create(first_name: "Luc", last_name: "Borris", hourly_rate: 0)
-@store2.employees.create(first_name: "Caroline", last_name: "Aube", hourly_rate: 0)
-@store2.employees.create(first_name: "Melanie", last_name: "Herbert", hourly_rate: 0)
+@store1.employees.create(first_name: "Luc", last_name: "Borris", hourly_rate: 40)
+@store2.employees.create(first_name: "Caroline", last_name: "Aube", hourly_rate: 60)
+@store2.employees.create(first_name: "Melanie", last_name: "Herbert", hourly_rate: 100)
